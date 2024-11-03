31a9e38f292151121189fcb143fd1208ba1b46a4
VoltagedDebunked <rusindanilo@gmail.com>
2024-11-03 23:13:59 +0200

    Fix kernel not loading correctly

--- src/boot.asm
+++ src/boot.asm
@@ -1,8 +1,7 @@
 ; boot.asm
 MBALIGN     equ  1<<0
 MEMINFO     equ  1<<1
-GRAPHICS    equ  1<<2
-FLAGS       equ  MBALIGN | MEMINFO | GRAPHICS
+FLAGS       equ  MBALIGN | MEMINFO
 MAGIC       equ  0x1BADB002
 CHECKSUM    equ -(MAGIC + FLAGS)
 
@@ -11,11 +10,6 @@ align 4
     dd MAGIC
     dd FLAGS
     dd CHECKSUM
-    dd 0, 0, 0, 0, 0        ; unused
-    dd 0                    ; graphics mode
-    dd 320                  ; width
-    dd 200                  ; height
-    dd 8                    ; bpp (bits per pixel)
 
 section .bss
 align 16
--- src/kernel.c
+++ src/kernel.c
@@ -2,8 +2,6 @@ typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 
-void kernel_main();
-
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 25
 #define VGA_MEMORY ((uint16_t*)0xB8000)
@@ -27,80 +25,25 @@ void vga_print(const char* str) {
     }
 }
 
-#define HEAP_START 0x100000
-#define HEAP_SIZE 0x10000
-unsigned int heap_end = HEAP_START;
-
-void* malloc(unsigned int size) {
-    if (heap_end + size > HEAP_START + HEAP_SIZE) return 0;
-    void* ptr = (void*)heap_end;
-    heap_end += size;
-    return ptr;
-}
-
-enum SyscallNumbers { SYS_PRINT, SYS_EXIT };
-
-void syscall(int number, void* arg) {
-    switch (number) {
-        case SYS_PRINT:
-            vga_print((const char*)arg);
-            break;
-        case SYS_EXIT:
-            while (1) {}
-    }
-}
-
-char keyboard_map[128] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0, 0,
-    0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0, 0,
-    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0, 0,
-    0, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-};
-
-static inline void outb(unsigned short port, unsigned char value) {
-    asm volatile("outb %0, %1" : : "a"(value), "Nd"(port));
-}
-
-static inline unsigned char inb(unsigned short port) {
-    unsigned char ret;
-    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
-    return ret;
-}
-
-void keyboard_handler() {
-    unsigned char scancode = inb(0x60);
-    if (scancode < 128 && keyboard_map[scancode] != 0) {
-        char key = keyboard_map[scancode];
-        vga_print(&key);
-    }
-}
-
-void mouse_handler() {
-    unsigned char status = inb(0x64);
-    if (status & 1) {
-        unsigned char mouse_data = inb(0x60);
-        if (mouse_data & 0x08) {
-            vga_print("Mouse moved\n");
-        }
-    }
-}
-
-void init_devices() {
-    outb(0x60, 0xAE);
-    outb(0x60, 0xF4);
-}
-
-void kernel_main() {
+void init_vga() {
     for (unsigned int i = 0; i < VGA_WIDTH * VGA_HEIGHT; i++) {
         VGA_MEMORY[i] = (WHITE_ON_BLACK << 8) | ' ';
     }
+    cursor_position = 0;
+}
 
-    vga_print("Initialized NoirOS Kernel. Entering userspace..\n");
+void kernel_main() {
+    // Write directly to video memory as a test
+    volatile unsigned char *video = (volatile unsigned char*)0xB8000;
+    video[0] = 'X';
+    video[1] = 0x0F;  // White on black
 
-    init_devices();
+    init_vga();
+    vga_print("Initialized NoirOS Kernel.\n");
+    vga_print("Welcome to NoirOS!\n");
 
-    syscall(SYS_EXIT, 0);
+    while(1) {
+        // Kernel main loop
+        asm volatile("hlt");
+    }
 }
